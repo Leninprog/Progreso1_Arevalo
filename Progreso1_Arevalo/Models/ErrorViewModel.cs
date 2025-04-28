8e03da7c64ec938975e04aa71ba17e5e8301d6d3
laboratorio <laboratorio@UPOAULA10702>
2025-04-28 07:15:43 -0500

    Agregar archivos de proyecto.

---
+++ Progreso1_Arevalo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Progreso1_Arevalo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
